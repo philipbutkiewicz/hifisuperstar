c577e518714698302be21ac5dde4de177a7292a1
Philip Butkiewicz <philip@kenja.com>
2023-04-21 20:21:19 +0200

    aa

--- hifisuperstar/core/Music/MediaSourceProcessing/YouTube.py
+++ hifisuperstar/core/Music/MediaSourceProcessing/YouTube.py
@@ -83,9 +83,9 @@ def get_ydl_opts(query=None):
     cache_tpl = f"cache/{str_hash_sha256(query)}" if query is not None else ''
     if os.path.exists(os.path.join('normalized', cache_tpl)):
         cache_tpl = os.path.join('normalized', cache_tpl)
-        warn(self, f'Normalized file in "{cache_tpl}" does exists')
+        warn(None, f'Normalized file in "{cache_tpl}" does exists')
     else:
-        warn(self, f'Normalized file in "{cache_tpl}" does not exist!')
+        warn(None, f'Normalized file in "{cache_tpl}" does not exist!')
     return {
         'format': 'm4a/bestaudio/best',
         'postprocessors': [{
